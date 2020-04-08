6828fb3b452217e8e745a622626d3d94613ffc7b
Alex McAusland <alex.mcausland@gmail.com>
2020-04-08 13:49:22 +0100

    Split checkstyle into own task

--- src/test/java/uk/gov/hmcts/JavaPluginTest.java
+++ src/test/java/uk/gov/hmcts/JavaPluginTest.java
@@ -8,14 +8,10 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
 
-/**
- * A simple unit test for the 'uk.gov.hmcts.greeting' plugin.
- */
 public class JavaPluginTest {
     @Ignore
     @Test
     public void pluginRegistersATask() {
-        // Create a test project and apply the plugin
         Project project = ProjectBuilder.builder().build();
         project.getPlugins().apply("uk.gov.hmcts.java");
 
