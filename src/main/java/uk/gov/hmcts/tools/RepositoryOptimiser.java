7b756dbc555a4a8eb2c542c36b6a75fc0fb54b9a
Jason Paige <jason.r.paige@gmail.com>
2022-03-17 13:41:34 +0000

    Ensure priority order is mavenLocal -> mavenCentral -> jitpack

--- src/main/java/uk/gov/hmcts/tools/RepositoryOptimiser.java
+++ src/main/java/uk/gov/hmcts/tools/RepositoryOptimiser.java
@@ -5,11 +5,16 @@ import org.gradle.api.artifacts.repositories.ArtifactRepository;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 
 
 public class RepositoryOptimiser {
 
-    private static final String PRIORITY_REPO_NAME = "mavenlocal";
+    private static final HashMap<String, Integer> REPO_PRIORITY = new HashMap<String, Integer>() {{
+            put("MavenLocal", 0);
+            put("MavenRepo", 1);
+            put("maven", 2);
+        }};
 
     private RepositoryOptimiser() {
     }
@@ -17,11 +22,21 @@ public class RepositoryOptimiser {
     public static void apply(Project project) {
 
         project.afterEvaluate(evaluatedProject -> {
+
             var repositoryHandler = evaluatedProject.getRepositories();
 
             ArtifactRepository[] repos = new ArtifactRepository[repositoryHandler.size()];
             repos = repositoryHandler.toArray(repos);
-            Arrays.sort(repos, (a, b) -> a.getName().equalsIgnoreCase(PRIORITY_REPO_NAME) ? -1 : 1);
+            Arrays.sort(repos, (a, b) -> {
+                if (REPO_PRIORITY.containsKey(a.getName()) && REPO_PRIORITY.containsKey(b.getName())) {
+                    return REPO_PRIORITY.get(a.getName()) < REPO_PRIORITY.get(b.getName()) ? -1 : 1;
+                } else if (REPO_PRIORITY.containsKey(a.getName())) {
+                    return -1;
+                } else if (REPO_PRIORITY.containsKey(b.getName())) {
+                    return 1;
+                }
+                return 0;
+            });
 
             repositoryHandler.clear();
             Collections.addAll(repositoryHandler, repos);
