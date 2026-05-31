7f201042c277444747c1d840a1ab1ed8661e6305
Michael1142 <michael1142@hotmail.co.uk>
2026-05-31 13:34:08 +0100

    Update dependency check integration tests to match new format

--- src/integrationTest/groovy/uk/gov/hmcts/IntegrationTest.groovy
+++ src/integrationTest/groovy/uk/gov/hmcts/IntegrationTest.groovy
@@ -9,11 +9,14 @@ import spock.lang.TempDir
 
 class IntegrationTest extends Specification {
     @TempDir
-    File projectFolder;
+    File projectFolder
+
     File buildFile
+    File settingsFile
 
     void setup() {
-        buildFile = projectFolder.newFile('build.gradle')
+        buildFile = new File(projectFolder, "build.gradle")
+        settingsFile = new File(projectFolder, "settings.gradle")
     }
 
     def "Check runs checkstyle against all sourcesets"() {
@@ -23,11 +26,13 @@ class IntegrationTest extends Specification {
                 id 'java-library'
                 id 'uk.gov.hmcts.java'
             }
+
             sourceSets {
                 functionalTest {
                 }
             }
         """
+
         when:
         def taskPaths = runner("check")
             .build()
@@ -47,6 +52,7 @@ class IntegrationTest extends Specification {
                 id 'java-library'
                 id 'uk.gov.hmcts.java'
             }
+    
             sourceSets {
                 integrationTest {
                 }
@@ -56,19 +62,15 @@ class IntegrationTest extends Specification {
                 }
             }
         """
+
         when:
-        // Expect build failure or success depending on provided Gradle property.
         def result = runner("dependencyCheckAnalyze")
                 .build()
 
         then:
-        result.output =~ "Analyzing.+:runtimeClasspath\\s"
-        !(result.output =~ "Analyzing.+:integrationTest\\s")
-        !(result.output =~ "Analyzing.+:functionalTest\\s")
-        !(result.output =~ "Analyzing.+:smokeTest\\s")
-        !(result.output =~ "Analyzing.+:pmd\\s")
-        !(result.output =~ "Analyzing.+:checkstyle\\s")
-        !(result.output =~ "Analyzing.+:compileOnly\\s")
+        result.output.contains("Verifying dependencies for project")
+        result.output.contains("Analysis Complete")
+        new File(projectFolder, "build/reports/dependency-check-report.xml").exists()
     }
 
     def "Dependency check fails build by default"() {
@@ -78,29 +80,31 @@ class IntegrationTest extends Specification {
                 id 'java-library'
                 id 'uk.gov.hmcts.java'
             }
+
             repositories {
                 mavenCentral()
             }
 
             dependencies {
                 // Known to have a CVE that should be detected by dependency checker.
-                compile group: 'com.fasterxml.jackson.core', name: 'jackson-databind', version: '2.7.0'
+                implementation group: 'com.fasterxml.jackson.core', name: 'jackson-databind', version: '2.7.0'
             }
         """
+
         when:
-        // Expect build failure or success depending on provided Gradle property.
-        def result = runner((["dependencyCheckAnalyze"]) as String[])
+        def result = runner("dependencyCheckAnalyze")
             .buildAndFail()
 
         then:
         result.output.contains("dependencies were identified with known vulnerabilities")
-        new File(projectFolder.getRoot(), 'build/reports/dependency-check-report.html').exists()
+        new File(projectFolder, 'build/reports/dependency-check-report.html').exists()
     }
 
-    def "Dependency check detects vulnerabilities in transient dependencies"() {
+    def "Dependency check detects vulnerabilities in transitive dependencies"() {
         given:
         File testLibrary = new File("test-projects/test-library")
-        FileUtils.copyDirectory(testLibrary, new File(projectFolder.getRoot(), "test-library"))
+        FileUtils.copyDirectory(testLibrary, new File(projectFolder, "test-library"))
+
         buildFile << """
             plugins {
                 id 'java-library'
@@ -113,15 +117,16 @@ class IntegrationTest extends Specification {
 
             dependencies {
                 // Known to have a CVE that should be detected by dependency checker.
-                compile group: 'org.springframework.cloud', name: 'spring-cloud-cloudfoundry-connector', version: '1.2.9.RELEASE'
+                implementation group: 'org.springframework.cloud', name: 'spring-cloud-cloudfoundry-connector', version: '1.2.9.RELEASE'
             }
         """
-        new File(projectFolder.getRoot(), 'settings.gradle') << """
+
+        settingsFile << """
             include 'test-library'
         """
 
         when:
-        def result = runner((["dependencyCheckAnalyze"]) as String[])
+        def result = runner("dependencyCheckAnalyze")
                 .buildAndFail()
 
         then:
@@ -130,12 +135,14 @@ class IntegrationTest extends Specification {
 
     GradleRunner runner(String... args) {
         ArrayList<String> arguments = Lists.newArrayList(args)
-        arguments.add("-is")
+        arguments.add("--info")
+        arguments.add("--stacktrace")
+
         return GradleRunner.create()
             .forwardOutput()
             .withPluginClasspath()
             .withArguments(arguments)
-            .withGradleVersion("5.0")
-            .withProjectDir(projectFolder.getRoot())
+            .withGradleVersion("9.0.0")
+            .withProjectDir(projectFolder)
     }
 }
